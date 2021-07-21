5e063d713479dfb383286469d2dcfdbfaeac8083
NPException <npexception42@gmail.com>
2021-07-21 19:52:39 +0200

    start work on oauth flow UI

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/data.json "2.4.0"]
                  [org.clojure/core.async "1.2.603"]
+                 [com.github.ben-manes.caffeine/caffeine "3.0.3"]
                  [http-kit "2.5.3"]
                  [metosin/reitit "0.5.11"]
                  [ring/ring-core "1.8.2"]
--- src/de/npcomplete/twtwlcm/core.clj
+++ src/de/npcomplete/twtwlcm/core.clj
@@ -1,28 +1,85 @@
 (ns de.npcomplete.twtwlcm.core
-  (:require [reitit.ring :as ring]
+  (:require [de.npcomplete.twtwlcm.twitter-api :as twitter-api]
+            [reitit.ring :as ring]
             [reitit.coercion.spec :as rspec]
             [reitit.ring.coercion :as coercion]
-            [ring.middleware.json :as mw-json]
             [ring.middleware.params :as mw-params]
-            [ring.adapter.jetty :as jetty])
-  (:gen-class))
+            [ring.middleware.cookies :as mw-cookies]
+            [org.httpkit.server :as server])
+  (:gen-class)
+  (:import (com.github.benmanes.caffeine.cache Cache Caffeine)
+           (java.util.concurrent TimeUnit)
+           (java.util UUID)
+           (java.util.function Function)))
 
-(defn ^:private get-welcome-messages
-  "Loads and returns the user's DM welcome messages"
-  [user]
-  ["I'm just a placeholder."])
+(def session-duration-minutes 30)
+
+(def ^Cache sessions (-> (Caffeine/newBuilder)
+                         (.expireAfterAccess session-duration-minutes TimeUnit/MINUTES)
+                         (.build)))
+
+(defn ^:private cookie-value [req key]
+  (-> (:cookies req) (get key) :value))
+
+;; TODO wrap session in middleware, but maybe don't create it by default
+(defn ^:private get-session
+  "Returns an active session if present."
+  [req]
+  (when-let [session-id (cookie-value req "session_id")]
+    (.getIfPresent sessions session-id)))
+
+(defn ^:private init-session
+  "Returns an active session or initializes a new one if not present."
+  [req]
+  (let [session-id (-> (cookie-value req "session_id")
+                       (or (.toString (UUID/randomUUID))))]
+    (.get sessions session-id
+          (reify Function
+            (apply [_ id] {:id id})))))
+
+(defn ^:private store-session!
+  [session]
+  (.put sessions (:id session) session))
+
+
+(defn home-handler
+  [req]
+  (println "Home")
+  (let [session (init-session req)]
+    ;; TODO: check if session has twitter access-token. If not, show link to /authenticate
+    {:status 200
+     :headers {"Content-Type" "text/html; charset=UTF-8"}
+     :cookies {"session_id" {:value (:id session)
+                             :max-age (* session-duration-minutes 60)
+                             :http-only true}}
+     :body (str "<a href=\"/authenticate\">Click me!</a>")}))
+
+
+(defn oauth-callback-handler
+  [req]
+  (let [session (init-session req)
+        request-token (:oauth-request-token session)
+        {:strs [oauth_token oauth_verifier]} (:query-params req)]
+    (when (= oauth_token (:oauth_token request-token))
+      (let [access-token (twitter-api/acquire-access-token! request-token oauth_verifier)
+            session (-> session
+                        (assoc :oauth-access-token access-token)
+                        (dissoc :oauth-request-token))]
+        (store-session! session)))
+    {:status 302
+     :headers {"Location" "/"}}))
 
 
 (def ^:private router
   (ring/ring-handler
     (ring/router
-      ["/get-welcome-messages"
-       {:get {:parameters {:query {:user string?}}
-              :responses {200 {:body vector?}}
-              :handler (fn [{{{:keys [user]} :query} :parameters}]
-                         {:status 200
-                          :headers {"Access-Control-Allow-Origin" "*"}
-                          :body (get-welcome-messages user)})}}]
+      [["/" {:get {:handler home-handler}}]
+       ;; TODO: start 3-legged oauth, and redirect to twitter
+       ["/authenticate"]
+       ["/oauth" {:get {:parameters {:query {:oauth_token string?
+                                             :oauth_verifier string?}}
+                        :response {302 {:headers {"Location" string?}}}
+                        :handler oauth-callback-handler}}]]
       {:data {:coercion rspec/coercion
               :middleware [coercion/coerce-exceptions-middleware
                            coercion/coerce-request-middleware
@@ -33,8 +90,10 @@
   [handler]
   (fn
     ([request]
-     (or (handler request)
-         {:status 400 :body "bad request"}))
+     (let [resp (handler request)]
+       (if (seq resp)
+         resp
+         {:status 400 :body "bad request"})))
     ([request respond raise]
      (handler request
               (fn [response]
@@ -45,17 +104,18 @@
 (defn -main
   "starts the server on a given :port (default 8080)"
   [& {:keys [port]
-      :or   {port 8080}}]
-  (jetty/run-jetty
+      :or {port 8080}}]
+  (server/run-server
     ((comp mw-params/wrap-params
-           wrap-empty-response
-           mw-json/wrap-json-response)
+           mw-cookies/wrap-cookies
+           wrap-empty-response)
      #'router)
-    {:port  port
-     :join? false}))
+    {:port port}))
 
 ;; development REPL snippets
 (comment
+  ;; start
   (def server (-main))
-  (.stop server)
+  ;; stop
+  (server)
   )
